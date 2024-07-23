8e3414c710f80d59b1d9c04816e7b693a36050e6
Hassan Murtaza <hassan.murtaza@smartfunstudios.com>
2024-07-23 08:45:39 +0500

    getAllInfo controller created

--- Controllers/controllers.js
+++ Controllers/controllers.js
@@ -21,3 +21,50 @@ exports.findinfo = async (req, res) => {
     error;
   }
 };
+
+/**
+ * Get All Info
+ */
+exports.getAllInfo = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const user = await Person.findOne({ _id: id });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    if (Work !== "Maneger") {
+      return res.status(400).json({
+        success: false,
+        message:
+          "You are invalid person to gett all information about chef and waiter",
+      });
+    }
+
+    const data = await Person.find({});
+
+    if (allData.length === 0) {
+      return res.status(200).josn({
+        success: true,
+        message: "Collection is empty",
+        data,
+      });
+    }
+
+    return res.status(200).josn({
+      success: true,
+      message: "Collection fetched successfully",
+      data,
+    });
+  } catch (error) {
+    console.error("An error occurred while gettiong info for all");
+    return res.status(500).json({
+      success: false,
+      message: "An error while getting data",
+    });
+  }
+};
